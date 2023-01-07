fbb2e6278af412eb5c624607e2ea52628b5ab512
Tom Maier <tom02.me14@gmail.com>
2023-01-07 11:55:55 +0100

    swapped \'\' to  " add Header to fix error in curl command

--- music_token.py
+++ music_token.py
@@ -6,7 +6,7 @@ secret = """-----BEGIN PRIVATE KEY-----
 ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ0123
 -----END PRIVATE KEY-----"""
 keyId = "0123456789"
-teamId = "9876543210"
+teamId = "1234567890"
 alg = 'ES256'
 
 time_now = datetime.datetime.now()
@@ -19,8 +19,8 @@ headers = {
 
 payload = {
 	"iss": teamId,
-	"exp": int(time_expired.timestamp()),
-	"iat": int(time_now.timestamp())
+	"iat": int(time_now.timestamp()),
+	"exp": int(time_expired.timestamp())
 }
 
 
@@ -32,5 +32,5 @@ if __name__ == "__main__":
 	print(token)
 
 	print("----CURL----")
-	print("curl -v -H 'Authorization: Bearer %s' \"https://api.music.apple.com/v1/catalog/us/artists/36954\" " % (token))
+	print("curl -v -H \"Authorization: Bearer %s\" \"https://api.music.apple.com/v1/catalog/us/artists/36954\" " % (token))
 
